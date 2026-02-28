d75ae17d2af8038eb56b518862b83473ee5260b4
AaronRuizzz <aaron.r.m@um.es>
2026-02-28 11:06:35 +0100

    Inicialización clase HistorialMovimientos

---
+++ core/src/main/java/com/tasku/core/domain/HistorialMovimientos.java
@@ -0,0 +1,12 @@
+package com.tasku.core.domain;
+
+import java.time.LocalDateTime;
+
+public class HistorialMovimientos {
+    private String id;
+    private LocalDateTime fechaHora;
+    private String accionDetalle;
+    private Email autor;
+
+    // Getters, setters, constructor, equals, hashCode
+}
