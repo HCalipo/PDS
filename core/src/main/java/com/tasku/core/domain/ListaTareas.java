2286beef5daa122f58886aae8faed393b8fb41e3
AaronRuizzz <aaron.r.m@um.es>
2026-02-28 11:07:44 +0100

    Inicialización clase ListaTareas

---
+++ core/src/main/java/com/tasku/core/domain/ListaTareas.java
@@ -0,0 +1,11 @@
+package com.tasku.core.domain;
+
+import java.util.List;
+
+public class ListaTareas {
+    private String id;
+    private int orden;
+    private List<Tarjeta> tarjetas;
+
+    // Getters, setters, constructor, equals, hashCode
+}
