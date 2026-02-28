340115a23ab1c8fb70f769e7401bcde111cdeebe
AaronRuizzz <aaron.r.m@um.es>
2026-02-28 11:07:09 +0100

    Inicialización clase Etiqueta

---
+++ core/src/main/java/com/tasku/core/domain/Etiqueta.java
@@ -0,0 +1,9 @@
+package com.tasku.core.domain;
+
+public class Etiqueta {
+    private String id;
+    private String texto;
+    private ColorEtiqueta color;
+
+    // Getters, setters, constructor, equals, hashCode
+}
