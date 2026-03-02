dfc001ea1d87f48e4d5a3fe318e4e51fee01ea40
AaronRuizzz <aaron.r.m@um.es>
2026-03-02 18:07:45 +0100

    Corrección de los value objects como record, así no se necesita redefinir equals y otros métodos - Email

--- core/src/main/java/com/tasku/core/domain/Email.java
+++ core/src/main/java/com/tasku/core/domain/Email.java
@@ -1,34 +1,4 @@
 package com.tasku.core.domain;
 
-import java.util.Objects;
-
-public class Email {
-    private final String value;
-
-    public Email(String value) {
-        // Aquí puedes agregar validación de formato
-        this.value = value;
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Email email = (Email) o;
-        return value.equals(email.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return value;
-    }
+public record Email(String value) {
 }
