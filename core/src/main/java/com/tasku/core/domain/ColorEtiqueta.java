695ea9e2ceaa0045c4e46d9dca7e6f4b231076fe
AaronRuizzz <aaron.r.m@um.es>
2026-03-13 22:55:08 +0100

    CODE - Simplificación del VO ColorEtiqueta

--- core/src/main/java/com/tasku/core/domain/ColorEtiqueta.java
+++ core/src/main/java/com/tasku/core/domain/ColorEtiqueta.java
@@ -2,29 +2,9 @@ package com.tasku.core.domain;
 
 public record ColorEtiqueta(String color) {
 	public ColorEtiqueta {
-		
-        if (color == null || color.isBlank()) {
-			throw new IllegalArgumentException("El color no puede ser nulo ni vacío");
+		if (color == null || color.isBlank()) {
+			throw new IllegalArgumentException("El color no puede ser nulo ni vacio");
 		}
-
-		color = color.trim().toLowerCase();
-
-        // Comprobamos si los colores están en hexadecimal
-		boolean esHex = color.matches("^#[0-9a-f]{6}$");
-        // Si no lo están, comprobamos que estén bien escritos (en inglés) y estén en permitidos
-		boolean esNombrePermitido = esNombrePermitido(color);
-
-		if (!esHex && !esNombrePermitido) {
-			throw new IllegalArgumentException("Color inválido. Usa un nombre permitido o formato HEX #rrggbb");
-		}
-	}
-
-	private static boolean esNombrePermitido(String color) {
-		try {
-			NombreColorEtiqueta.desde(color);
-			return true;
-		} catch (IllegalArgumentException ex) {
-			return false;
-		}
+		color = color.trim();
 	}
 }
