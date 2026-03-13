3841133b8dbb436ac8ae24d264059310ebc4f6b1
AaronRuizzz <aaron.r.m@um.es>
2026-03-13 21:34:59 +0100

    CODE - Implementación de el VO ElementoChecklist

--- core/src/main/java/com/tasku/core/domain/ElementoChecklist.java
+++ core/src/main/java/com/tasku/core/domain/ElementoChecklist.java
@@ -1,4 +1,11 @@
 package com.tasku.core.domain;
 
 public record ElementoChecklist(String descripcion, boolean estaMarcado) {
+	public ElementoChecklist {
+		if (descripcion == null || descripcion.isBlank()) {
+			throw new IllegalArgumentException("La descripción no puede ser nula ni vacía");
+		}
+
+		descripcion = descripcion.trim();
+	}
 }
