305e9bb0771110a81f5ace3bb26b70a23835761c
AaronRuizzz <aaron.r.m@um.es>
2026-03-15 12:17:12 +0100

    CODE -TarjetaChecklist, constructor, getter y funciones para agregar items o marcar/desmarcar un item

--- core/src/main/java/com/tasku/core/domain/TarjetaChecklist.java
+++ core/src/main/java/com/tasku/core/domain/TarjetaChecklist.java
@@ -1,7 +1,33 @@
 package com.tasku.core.domain;
 
+import java.util.Objects;
+
 public class TarjetaChecklist extends Tarjeta {
     private ListaItems listaItems;
 
-    // Getters, setters, constructor, equals, hashCode
+    // Constructores 
+
+    public TarjetaChecklist(TarjetaId id, String titulo, String descripcion, ListaItems listaItems) {
+        super(id, titulo, descripcion);
+        this.listaItems = Objects.requireNonNull(listaItems, "La lista de items no puede ser nula");
+    }
+
+    public TarjetaChecklist(String titulo, String descripcion) {
+        this(new TarjetaId(), titulo, descripcion, new ListaItems());
+    }
+
+    // Getter
+    public ListaItems getListaItems() {
+        return listaItems;
+    }
+
+    // Función para agregar items a la lista de checklist
+    public void agregarItemChecklist(String descripcion) {
+        listaItems.agregarItem(new ElementoChecklist(descripcion, false));
+    }
+
+    // Función para marcar o desmarcar items de la lista de checklist
+    public void marcarItemChecklist(int indice, boolean marcado) {
+        listaItems.marcarItem(indice, marcado);
+    }
 }
